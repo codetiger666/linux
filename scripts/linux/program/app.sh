0ce7587bb38574c08f3211b78c54b9a26e4aa6de
codetiger <codetiger806@gmail.com>
2023-11-28 20:04:28 +0800

    添加linux应用启动脚本

--- scripts/linux/program/app.sh
+++ scripts/linux/program/app.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 # 应用名称
-APP_NAME=blog-view
+APP_NAME=app
 # 应用运行目录
 PROGRAM_PATH=(dirname "$(realpath "$0")")
-# jvm参数
-JAVA_OPS="-Xmx256m -Xms128m -XX:MaxMetaspaceSize=128m"
-# java 命令行参数
-JAVA_COMMAND_LINE="--server.port=20500 --spring.profiles.active=pro"
-# java安装位置
-JAVA_HOME=/usr/local/jdk11
 
 case "$1" in
 start)
-    $JAVA_HOME/bin/java $JAVA_OPS -jar $PROGRAM_PATH/$APP_NAME.jar $JAVA_COMMAND_LINE > $PROGRAM_PATH/$APP_NAME.log 2>&1 &
+    $PROGRAM_PATH/$APP_NAME > $PROGRAM_PATH/$APP_NAME.log 2>&1 &
     echo $! > $PROGRAM_PATH/$APP_NAME.pid
     ;;
 stop)
